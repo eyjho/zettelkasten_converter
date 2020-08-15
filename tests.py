c1323e42a64abf440994c44cc870c581ebd3db0a
eyjho <eyjho@users.noreply.github.com>
2020-08-15 12:57:45 +0100

    Test split into 3 suites
    Suites: init, csv import, txt import

--- tests.py
+++ tests.py
@@ -9,66 +9,74 @@ https://realpython.com/python-testing/#writing-your-first-test
 import unittest
 from zettelkasten_txt_to_csv.zettelkasten_txt_to_csv import Zettelkasten
 
-# class TestZettelkasten(unittest.TestCase):
-# 	'''Unit test for key functions of zettelkasten converter'''
-
-# 	def setUp(self):
-# 		'''Run before every subsequent test'''
-# 		global zkn
-# 		zkn = Zettelkasten(diagnostics = False)
-# 		self.library_length = 73
-
-# 	def test_template(self):
-# 		'''Template "Zettelkasten v0_2.csv'''
-# 		result = ''
-# 		correct_answer = ''
-# 		self.assertEqual(result, correct_answer)
-
-# 	def test_find_file(self):
-# 		'''Select "Zettelkasten v0_2.csv and return file_path'''
-# 		result = zkn.find_file()
-# 		correct_answer = 'C:/Users/Eugene/Documents\
-# /GitHub/zettelkasten_txt_to_csv/data/Zettelkasten v0_2.csv'
-# 		self.assertEqual(result, correct_answer)
-
-# 	def test_extract_filepath(self):
-# 		'''Find filepath without ending'''
-# 		# print('file_path: ', zkn.file_path) # zkn.file_path empty in test but fine in operation
-# 		file_path = 'C:/Users/Eugene/Documents\
-# /GitHub/zettelkasten_txt_to_csv/data/Zettelkasten v0_2.csv'
-# 		result = zkn.extract_filepath(file_path)
-# 		correct_answer = 'C:/Users/Eugene/Documents\
-# /GitHub/zettelkasten_txt_to_csv/data/Zettelkasten v0_2'
-# 		self.assertEqual(result, correct_answer)
-
-# 	def test_import_csv_length(self):
-# 		'''Length of imported csv library should match row count'''
-# 		file_path = 'C:/Users/Eugene/Documents\
-# /GitHub/zettelkasten_txt_to_csv/data/Zettelkasten v0_2.csv'
-# 		result = len(zkn.import_csv_zk(file_path = file_path))
-# 		correct_answer = self.library_length
-# 		self.assertEqual(result, correct_answer)
-
-# 	def test_import_txt_length(self):
-# 		'''Length of imported txt library should match row count'''
-# 		file_path = 'C:/Users/Eugene/Documents\
-# /GitHub/zettelkasten_txt_to_csv/data/00 Gardening zettlelkasten.txt'
-# 		result = len(zkn.import_txt_zk(file_path = file_path))
-# 		correct_answer = self.library_length
-# 		self.assertEqual(result, correct_answer)
-
-# 	def test_duplicates(self):
-# 		'''Check keys are unique'''
-# 		result = len(zkn.library)
-# 		correct_answer = len(set(zkn.library))
-# 		self.assertEqual(result, correct_answer)
-
-# 	def tearDown(self):
-# 		'''Run after every component test'''
-# 		pass
-
-class TestZkn_Txt(unittest.TestCase):
+class TestZettelkasten(unittest.TestCase):
 	'''Unit test for key functions of zettelkasten converter'''
+
+	def setUp(self):
+		'''Run before every subsequent test'''
+		global zkn
+		zkn = Zettelkasten(diagnostics = False)
+		self.library_length = 73
+
+	def test_template(self):
+		'''Template "Zettelkasten v0_2.csv'''
+		result = ''
+		correct_answer = ''
+		self.assertEqual(result, correct_answer)
+
+	def test_find_file(self):
+		'''Select "Zettelkasten v0_2.csv and return file_path'''
+		result = zkn.find_file()
+		correct_answer = 'C:/Users/Eugene/Documents\
+/GitHub/zettelkasten_txt_to_csv/data/Zettelkasten v0_2.csv'
+		self.assertEqual(result, correct_answer)
+
+	def test_extract_filepath(self):
+		'''Find filepath without ending'''
+		# print('file_path: ', zkn.file_path) # zkn.file_path empty in test but fine in operation
+		file_path = 'C:/Users/Eugene/Documents\
+/GitHub/zettelkasten_txt_to_csv/data/Zettelkasten v0_2.csv'
+		result = zkn.extract_filepath(file_path)
+		correct_answer = 'C:/Users/Eugene/Documents\
+/GitHub/zettelkasten_txt_to_csv/data/Zettelkasten v0_2'
+		self.assertEqual(result, correct_answer)
+
+	def tearDown(self):
+		'''Run after every component test'''
+		global zkn
+		del zkn
+
+class TestZkn_CSV(unittest.TestCase):
+	'''Unit test for csv import of zettelkasten converter'''
+
+	def setUp(self):
+		'''Run before every subsequent test'''
+		global zkn
+		zkn = Zettelkasten(diagnostics = False)
+		file_path = 'C:/Users/Eugene/Documents\
+/GitHub/zettelkasten_txt_to_csv/data/Zettelkasten v0_2.csv'
+		zkn.library = zkn.import_csv_zk(file_path = file_path)
+		self.library_length = 73
+
+	def test_import_csv_length(self):
+		'''Length of imported txt library should match row count'''
+		result = len(zkn.library)
+		correct_answer = self.library_length
+		self.assertEqual(result, correct_answer)
+
+	def test_duplicates(self):
+		'''Check keys are unique'''
+		result = len(zkn.library)
+		correct_answer = len(set(zkn.library))
+		self.assertEqual(result, correct_answer)
+
+	def tearDown(self):
+		'''Run after every component test'''
+		global zkn
+		del zkn
+
+class TestZkn_TXT(unittest.TestCase):
+	'''Unit test for text import of zettelkasten converter'''
 
 	def setUp(self):
 		'''Run before every subsequent test'''
