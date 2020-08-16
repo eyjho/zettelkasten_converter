99ee722078e02c8c28dc80291bbfe7ce64538e04
eyjho <eyjho@users.noreply.github.com>
2020-08-16 11:40:55 +0100

    Refactor import zettelkasten to strategy
    Define functions in zettelkasten, but extract and run within strategy

--- zettelkasten_txt_to_csv/strategy.py
+++ zettelkasten_txt_to_csv/strategy.py
@@ -1,7 +1,8 @@
 """Zettelkasten converter
 Eugene Ho, 16 Aug 2020
 
-Implmenting strategy pattern
+Implmenting strategy pattern from 
+https://refactoring.guru/design-patterns/strategy/python/example
 
 Strategy is a behavioral design pattern. It enables an algorithm's behavior to
 be selected at runtime. We can implement it by creating a common (abstract)
@@ -10,67 +11,75 @@ in [1], or by creating a single class and replacing a method of that class with
 a different function, how it's done in [2]. The latter implementation is
 possible because in Python functions are first class objects.
 """
+
 from __future__ import annotations
 from typing import List
+import re
+import os
+import csv
+import time
+import tkinter as tk
+from tkinter import filedialog
+from datetime import datetime, timezone
+from zettelkasten_txt_to_csv import Zettelkasten
 
 class Context():
-    """
-    The Context defines the interface of interest to clients.
-    """
+	"""
+	The Context defines the interface of interest to clients.
+	"""
 
-    def __init__(self, strategy: Strategy) -> None:
-        """
-        Usually, the Context accepts a strategy through the constructor, but
-        also provides a setter to change it at runtime.
-        """
+	def __init__(self, strategy: Strategy) -> None:
+		"""
+		Usually, the Context accepts a strategy through the constructor, but
+		also provides a setter to change it at runtime.
+		"""
 
-        self._strategy = strategy
+		self._strategy = strategy
 
-    @property
-    def strategy(self) -> Strategy:
-        """
-        The Context maintains a reference to one of the Strategy objects. The
-        Context does not know the concrete class of a strategy. It should work
-        with all strategies via the Strategy interface.
-        """
+	@property
+	def strategy(self) -> Strategy:
+		"""
+		The Context maintains a reference to one of the Strategy objects. The
+		Context does not know the concrete class of a strategy. It should work
+		with all strategies via the Strategy interface.
+		"""
 
-        return self._strategy
+		return self._strategy
 
-    @strategy.setter
-    def strategy(self, strategy: Strategy) -> None:
-        """
-        Usually, the Context allows replacing a Strategy object at runtime.
-        """
+	@strategy.setter
+	def strategy(self, strategy: Strategy) -> None:
+		"""
+		Usually, the Context allows replacing a Strategy object at runtime.
+		"""
 
-        self._strategy = strategy
+		self._strategy = strategy
 
-    def do_some_business_logic(self) -> None:
-        """
-        The Context delegates some work to the Strategy object instead of
-        implementing multiple versions of the algorithm on its own.
-        """
+	def do_some_business_logic(self) -> None:
+		"""
+		The Context delegates some work to the Strategy object instead of
+		implementing multiple versions of the algorithm on its own.
+		"""
 
-        # ...
+		# ...
 
-        print("Context: Sorting data using the strategy (not sure how it'll do it)")
-        result = self._strategy.do_algorithm(["a", "b", "c", "d", "e"])
-        print(",".join(result))
+		print("Context: Sorting data using the strategy (not sure how it'll do it)")
+		result = self._strategy.do_algorithm(["a", "b", "c", "d", "e"])
+		print(",".join(result))
 
-        # ...
+		# ...
 
 
-class Strategy():
-    """
-    The Strategy interface declares operations common to all supported versions
-    of some algorithm.
+class Strategy(Zettelkasten):
+	"""
+	The Strategy interface declares operations common to all supported versions	of some algorithm.
 
-    The Context uses this interface to call the algorithm defined by Concrete
-    Strategies.
-    """
+	The Context uses this interface to call the algorithm defined by Concrete Strategies.
+	"""
 
-    # @abstractmethod
-    def do_algorithm(self, data: List):
-        pass
+	# @abstractmethod
+
+	def do_algorithm(self, data: List):
+		pass
 
 
 """
@@ -79,26 +88,26 @@ interface. The interface makes them interchangeable in the Context.
 """
 
 
-class ConcreteStrategyA(Strategy):
-    def do_algorithm(self, data: List) -> List:
-        return sorted(data)
+class import_csv(Strategy):
+	def do_algorithm(self, data: List) -> List:
+		return sorted(data)
 
 
-class ConcreteStrategyB(Strategy):
-    def do_algorithm(self, data: List) -> List:
-        return reversed(sorted(data))
+class import_txt(Strategy):
+	def do_algorithm(self, data: List) -> List:
+		return reversed(sorted(data))
 
 
 if __name__ == "__main__":
-    # The client code picks a concrete strategy and passes it to the context.
-    # The client should be aware of the differences between strategies in order
-    # to make the right choice.
+	# The client code picks a concrete strategy and passes it to the context.
+	# The client should be aware of the differences between strategies in order
+	# to make the right choice.
 
-    context = Context(ConcreteStrategyA())
-    print("Client: Strategy is set to normal sorting.")
-    context.do_some_business_logic()
-    print()
+	context = Context(import_csv())
+	print("Client: Strategy is set to normal sorting.")
+	context.do_some_business_logic()
+	print()
 
-    print("Client: Strategy is set to reverse sorting.")
-    context.strategy = ConcreteStrategyB()
-    context.do_some_business_logic()
+	print("Client: Strategy is set to reverse sorting.")
+	context.strategy = import_txt()
+	context.do_some_business_logic()
